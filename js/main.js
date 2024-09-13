6b7fb7da88b4cdf8abfff1632d6fbbcc2fe894a3
Bruno Vieira <vieirabruno28112005@gmail.com>
2024-09-13 17:30:16 -0300

    style

--- js/main.js
+++ js/main.js
@@ -1,23 +1,48 @@
+let cor = 'black'
 $("#home-tab").click(() => {
     $("#home").show("3s");
+
+    $("#home-tab").parent().css("background-color",cor);
+    $("#backbone-tab").parent().css("background-color",'');
+    $("#infra-tab").parent().css("background-color",'');
+    $("#sobre-tab").parent().css("background-color",'');
+
     $("#backbone").hide("3s");
     $("#infra").hide("3s");
     $("#sobre").hide("3s");
 });
 $("#backbone-tab").click(() => {
     $("#backbone").show("3s");
+
+    $("#backbone-tab").parent().css("background-color",cor);
+    $("#home-tab").parent().css("background-color",'');
+    $("#infra-tab").parent().css("background-color",'');
+    $("#sobre-tab").parent().css("background-color",'');
+
     $("#home").hide("3s");
     $("#infra").hide("3s");
     $("#sobre").hide("3s");
 });
 $("#infra-tab").click(() => {
     $("#infra").show("3s");
+
+    $("#infra-tab").parent().css("background-color",cor);
+    $("#backbone-tab").parent().css("background-color",'');
+    $("#home-tab").parent().css("background-color",'');
+    $("#sobre-tab").parent().css("background-color",'');
+
     $("#backbone").hide("3s");
     $("#home").hide("3s");
     $("#sobre").hide("3s");
 });
 $("#sobre-tab").click(() => {
     $("#sobre").show("3s");
+
+    $("#sobre-tab").parent().css("background-color",cor);
+    $("#backbone-tab").parent().css("background-color",'');
+    $("#infra-tab").parent().css("background-color",'');
+    $("#home-tab").parent().css("background-color",'');
+
     $("#backbone").hide("3s");
     $("#infra").hide("3s");
     $("#home").hide("3s");
