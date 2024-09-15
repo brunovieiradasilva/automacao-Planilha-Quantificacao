9e7c410d63b0b0286db1af4112bea344a4390a6b
Bruno Vieira <vieirabruno28112005@gmail.com>
2024-09-15 16:33:27 -0300

    tabelas

--- js/adicionar_planilha.js
+++ js/adicionar_planilha.js
@@ -1,4 +1,18 @@
 $("#salvar").click(function () {
-let c = document.querySelector('#container');
-let  lastChild = c.lastChild;
+
+    let planilha = document.querySelector("input[name='1']:checked");
+
+    let tipoPlanilha = null;
+    if (planilha.value == "t") {
+        tipoPlanilha = "total-table";
+    }
+    else if (planilha.value == "m") {
+        tipoPlanilha = "mh-table";
+    }
+    else {
+        tipoPlanilha = "backbone-table";
+    }
+
+    let c = document.querySelector('#container');
+    let lastChild = c.lastChild;
 });
