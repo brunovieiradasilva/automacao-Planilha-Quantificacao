e5e1b794aef06559b69818ad9799ff5a0b401306
Rudá Brandão <rudabrandao@ruoso.com>
2024-09-15 22:32:13 -0300

    errata

--- js/adicionar_planilha.js
+++ js/adicionar_planilha.js
@@ -7,8 +7,6 @@ var tipoCabo;
 
 const tamRack = [4, 6, 8, 10, 12, 14, 16, 20, 24, 28, 32, 36, 40, 44, 48]
 
-
-
 var contentBB = [
     `Distribuidor Interno Óptico - Chassi ( ${portasDIO} portas )`,
     `Caixa de Emendas ( ${fibras} fibras )`,
@@ -65,6 +63,11 @@ function gerarPlanilhaMH(tblBody) {
         UsTotais /= qtdRack;
     }
 
+    if (!tamRack.includes(UsTotais)) 
+        for (i=0; i<15; i++)
+            if (UsTotais > tamRack[i])
+                UsTotais = tamRack[i-1];    
+
     tblBody.appendChild(createRow( "Tomada RJ 45 Fêmea (categoria  6)", ptsTelecon * 2 + ptsRede ));
     tblBody.appendChild(createRow( "Cordão de ligação (Patch Cord), (categoria: 6), (Tamanho: 3m), (Cor: azul)", ptsTelecon * 2 + ptsRede - ptsCFTV ));
     if (ptsCFTV > 0)
@@ -109,6 +112,72 @@ function gerarPlanilhaMH(tblBody) {
 }
 
 function gerarPlanilhaBB() {
+
+    ptsTelecon = Number(document.querySelector("#pts-telecom").value);
+    ptsRede = Number(document.querySelector("#pts-rede").value);
+    ptsVOIP = Number(document.querySelector("#pts-voip").value);
+    ptsCFTV = Number(document.querySelector("#pts-cftv").value);
+    distMalha = Number(document.querySelector("#distanciaMalha").value);
+    qtdRack = 
+
+    PP = Math.ceil((ptsTelecon * 2 + ptsRede) / 24);
+
+    tipoRack = document.querySelector("input[name='tipoRack']:checked").value;
+    UsTotais = 4 * PP + 4;
+    if (tipoRack === "Fechado")     UsTotais += 2;
+    
+    UsTotais *= 1.5;
+    if (UsTotais > 48) {
+        qtdRack = Math.ceil(UsTotais / 48)
+        UsTotais /= qtdRack;
+    }
+
+    if (!tamRack.includes(UsTotais)) 
+        for (i=0; i<15; i++)
+            if (UsTotais > tamRack[i])
+                UsTotais = tamRack[i-1];    
+
+    tblBody.appendChild(createRow( "Tomada RJ 45 Fêmea (categoria  6)", ptsTelecon * 2 + ptsRede ));
+    tblBody.appendChild(createRow( "Cordão de ligação (Patch Cord), (categoria: 6), (Tamanho: 3m), (Cor: azul)", ptsTelecon * 2 + ptsRede - ptsCFTV ));
+    if (ptsCFTV > 0)
+        tblBody.appendChild(createRow( "Cordão de ligação (Patch Cord), (categoria: 6), (Tamanho: 3m), (Cor: parede)", ptsCFTV ));
+    tblBody.appendChild(createRow( "Espelho de conexão (Tamanho 2x4) (2 furações)", ptsTelecon ));
+    tblBody.appendChild(createRow( "Espelho de conexão (Tamanho 2x4) (1 furações)", ptsRede ));
+    tblBody.appendChild(createRow( "Cabo UTP  rígido (categoria:  6)", ptsTelecon * 2 + ptsRede ));
+    tblBody.appendChild(createRow( "PPMH (Patch Panel de Malha Horizontal)", PP ));
+    tblBody.appendChild(createRow( "Organizador frontal de cabo", (ptsTelecon * 2 + ptsRede) / 12 ));
+
+    if (ptsCFTV + ptsVOIP < (ptsTelecon * 2 + ptsRede))
+        tblBody.appendChild(createRow( "Cordão de Ligação, flexível, (Patch Cable), (categoria 6), (Tamanho: 2m),  (cor: azul)", ptsTelecon * 2 + ptsRede - ptsCFTV - ptsVOIP ));
+    if (ptsCFTV > 0)
+        tblBody.appendChild(createRow( "Cordão de Ligação, flexível, (Patch Cable), (categoria 6), (Tamanho: 2m),  (cor: vermelho)", ptsCFTV ));
+    if (ptsVOIP > 0)
+        tblBody.appendChild(createRow( "Cordão de Ligação, flexível, (Patch Cable), (categoria 6), (Tamanho: 2m),  (cor: amarelo)", ptsVOIP ));
+
+    tblBody.appendChild(createRow( `Rack ( ${tipoRack} ), (Tamanho: ${UsTotais} )`, qtdRack )); 
+    
+    if (tipoRack === "Fechado") {
+        tblBody.appendChild(createRow( "Organizador lateral para Rack", 2 ));
+        tblBody.appendChild(createRow( "Exaustor (Tamanho: 2U )", 2 ));  
+    }
+
+    tblBody.appendChild(createRow( "NVR (Tamanho: 2U )", Math.ceil(ptsCFTV/32) )); //COMPLETARCOMPLETARCOMPLETARCOMPLETARCOMPLETARCOMPLETAR
+    
+    tblBody.appendChild(createRow( 'Bandeja fixa', 1 ));
+    tblBody.appendChild(createRow( "Régua de Fechamento", 1234567890 )); //COMPLETARCOMPLETARCOMPLETARCOMPLETARCOMPLETARCOMPLETARCOMPLETAR
+    tblBody.appendChild(createRow( "Parafuso Porca Gaiola (conjunto com 10 unidades)", UsTotais * 4 ));
+    tblBody.appendChild(createRow( "Abraçadeira de velcro", 3 ));
+    tblBody.appendChild(createRow( "Abraçadeira Hellermann (conjunto com 100 unidades)", 1 )); //COMPLETARCOMPLETAR
+    tblBody.appendChild(createRow( "Filtro de linha com 06 tomadas", 1 )); //COMPLETARCOMPLETARCOMPLETARCOMPLETARCOMPLETARCOMPLETARCOMPLETAR
+    tblBody.appendChild(createRow( "Etiquetas para Rack", 1 )); //COMPLETARCOMPLETARCOMPLETARCOMPLETARCOMPLETARCOMPLETARCOMPLETARCOMPLETAR
+    tblBody.appendChild(createRow( "Etiquetas para Patch Panel", Math.ceil((ptsTelecon * 2 + ptsRede) / 24) ));
+    tblBody.appendChild(createRow( "Etiquetas de identificação de portas do Patch Panel", PP ));
+    tblBody.appendChild(createRow( "Etiquetas para identificação de Patch Cables", ptsTelecon * 2 + ptsRede ));
+    tblBody.appendChild(createRow( "Etiquetas para identificação de Patch Cables", ptsTelecon * 2 + ptsRede ));
+    tblBody.appendChild(createRow( "Etiqueta identificação do cabo de malha horizontal", (ptsTelecon * 2 + ptsRede) * 2 ));
+    tblBody.appendChild(createRow( "Etiquetas para identificação de tomadas e espelho", ptsTelecon * 3 + ptsRede * 2 ));
+
+    return tblBody;
 }
 
 function generateTable() {
